cec25a967fadfef0507fef52425fa0dae11fadf3
laksitha <laksithanayanajith@outlook.com>
2024-03-20 09:43:31 +0530

    Initial Commit

---
+++ LojoEcommerce/LojoEcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LojoEcommerceApp.swift
+//  LojoEcommerce
+//
+//  Created by NIBM-LAB04-PC05 on 2024-03-20.
+//
+
+import SwiftUI
+
+@main
+struct LojoEcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
