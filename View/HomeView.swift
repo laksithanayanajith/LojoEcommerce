062a286424044f6d9482004e138088a032302e00
laksitha <laksithanayanajith@outlook.com>
2024-03-20 12:49:19 +0530

    Added loading icon and modify the home view for show images

--- View/HomeView.swift
+++ View/HomeView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @State private var isLoading = true
     @State private var items: [ItemElement] = []
     
     var body: some View {
@@ -12,6 +13,10 @@ struct HomeView: View {
                     .fontWeight(.bold)
                     .multilineTextAlignment(.center)
                     .padding()
+                
+                if isLoading {
+                    ProgressView() // Show loading indicator if isLoading is true
+                }
                 
                 HStack {
                     VStack {
@@ -34,6 +39,7 @@ struct HomeView: View {
         .onAppear {
             Task {
                 await fetchItems { items in
+                    isLoading = false
                     if let items = items {
                         // Update the state variable with the fetched items
                         self.items = items
@@ -43,7 +49,7 @@ struct HomeView: View {
             
         }
     }
-    
+    /*
     struct RectangleItemView: View {
         let item: ItemElement
         
@@ -73,7 +79,55 @@ struct HomeView: View {
                 )
         }
     }
+    */
     
+    struct RectangleItemView: View {
+        let item: ItemElement
+        @State private var imageURL: String?
+        
+        var body: some View {
+            VStack {
+                if let imageURL = imageURL, let url = URL(string: imageURL) {
+                    AsyncImage(url: url) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } placeholder: {
+                        Rectangle()
+                            .foregroundColor(Color.gray.opacity(0.8))
+                    }
+                    .frame(minWidth: 100, maxWidth: 250, minHeight: 100, maxHeight: 150) // Adjust size as needed
+                } else {
+                    Rectangle()
+                        .foregroundColor(Color.gray.opacity(0.5))
+                        .frame(minWidth: 100, maxWidth: 250, minHeight: 100, maxHeight: 150) // Adjust size as needed
+                }
+                
+                VStack {
+                    Text(item.name)
+                        .font(.callout)
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .padding(.bottom, 5)
+                    Text(item.description)
+                        .font(.caption2)
+                        .foregroundColor(.white)
+                    Text("\n$" + String(format: "%.2f", item.price))
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                }
+                .padding()
+            }
+            .onAppear {
+                Task{
+                    await fetchImageURL(itemID: item.id) { imageURL in
+                        self.imageURL = imageURL
+                    }
+                }
+            }
+        }
+    }
+
     struct HomeView_Previews: PreviewProvider {
         static var previews: some View {
             HomeView()
