7bf9e7ce2343cea689ebb6d07f3cc3280f70ab13
laksitha <laksithanayanajith@outlook.com>
2024-03-20 09:47:48 +0530

    added MVVM and with welcome,home and item

--- LojoEcommerce/LojoEcommerceApp.swift
+++ LojoEcommerce/LojoEcommerceApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct LojoEcommerceApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                WelcomeView()
+            }
         }
     }
 }
---
+++ View/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  LojoEcommerce
+//
+//  Created by NIBM-LAB04-PC05 on 2024-03-20.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
