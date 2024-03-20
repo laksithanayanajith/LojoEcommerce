feec2f31ea7760caf17502fcdb3e5fb90e3e8982
laksitha <laksithanayanajith@outlook.com>
2024-03-20 09:47:48 +0530

    added MVVM and with welcome,home and item

---
+++ Model/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  LojoEcommerce
+//
+//  Created by NIBM-LAB04-PC05 on 2024-03-20.
+//
+
+import Foundation
---
+++ View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  LojoEcommerce
+//
+//  Created by NIBM-LAB04-PC05 on 2024-03-20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
