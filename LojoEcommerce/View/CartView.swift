48cc1e1dd51e6bc612f334fc30471436b2a13780
Laksitha <laksithanayanajith@outlook.com>
2024-03-29 09:27:06 +0530

    modifed isShowSelectedItem usage

--- LojoEcommerce/View/CartView.swift
+++ LojoEcommerce/View/CartView.swift
@@ -39,108 +39,109 @@ struct CartView: View {
 struct CartItemView: View {
     
     var selectedItem: CartSelectedItemElement
+    @State private var isShowSelectedItem: Bool = true
 
     var body: some View {
         
         VStack {
-            VStack{
-                HStack{
-                    VStack{
-                        if let name = selectedItem.item?.name {
-                            Text("\(name)\n")
+            if isShowSelectedItem {
+                VStack{
+                    HStack{
+                        VStack{
+                            if let name = selectedItem.item?.name {
+                                Text("\(name)\n")
+                                    .font(.footnote)
+                                    .fontWeight(.thin)
+                                    .frame(width: 150)
+                                    .padding(.horizontal)
+                            }
+                            
+                            Text("\(selectedItem.selectedSize)\n")
                                 .font(.footnote)
-                                .fontWeight(.thin)
-                                .frame(width: 150)
+                                .fontWeight(.medium)
+                                .frame(width: 100)
                                 .padding(.horizontal)
                         }
                         
-                        Text("\(selectedItem.selectedSize)\n")
-                            .font(.footnote)
-                            .fontWeight(.medium)
-                            .frame(width: 100)
-                            .padding(.horizontal)
-                    }
-                    
-                    VStack{
-                        Button(action: {
-                            
-                        }) {
-                            Text("+")
-                                .fontWeight(.bold)
-                                .foregroundColor(.black)
-                                .padding(5)
-                        }
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
-                        .frame(width: 50, height: 50)
-                        .cornerRadius(25)
-                        
-                        if let quantity = selectedItem.quantity {
-                            Text("\(quantity)")
-                                .font(.footnote)
-                                .fontWeight(.medium)
-                        }
-                        
-                        Button(action: {
-                            
-                        }) {
-                            Text("-")
-                                .fontWeight(.bold)
-                                .foregroundColor(.black)
-                                .padding(5)
-                        }
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
-                        .frame(width: 50, height: 50)
-                        .cornerRadius(25)
-                    }
-                }
-                
-                HStack{
-                    
-                    if let price = selectedItem.item?.price {
-                        Text("\(price)")
-                            .font(.headline)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            .alignmentGuide(.leading) { _ in -15 }
-                    }
-                }
-                
-                Button(action: {
-                    Task{
-                        if let id = selectedItem.id {
-                            await deleteSelectedItem(id: id) { success in
-                                if success {
-                                    print("Successfully deleted the item")
-                                } else {
-                                    // Handle deletion failure if needed
-                                    print("Failed to delete the item")
-                                }
+                        VStack{
+                            Button(action: {
+                                
+                            }) {
+                                Text("+")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .padding(5)
                             }
-                        } else {
-                            print("Error: selectedItem.id is nil")
-                            // Handle the case when selectedItem.id is nil
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(25)
+                            
+                            if let quantity = selectedItem.quantity {
+                                Text("\(quantity)")
+                                    .font(.footnote)
+                                    .fontWeight(.medium)
+                            }
+                            
+                            Button(action: {
+                            }) {
+                                Text("-")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .padding(5)
+                            }
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(25)
                         }
-                        
                     }
-                }) {
-                    Text("delete")
-                        .font(.footnote)
-                        .fontWeight(.bold)
-                        .foregroundColor(.red)
-                        .padding(5)
-                }
+                    
+                    HStack{
+                        
+                        if let price = selectedItem.item?.price {
+                            Text("\(price)")
+                                .font(.headline)
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .alignmentGuide(.leading) { _ in -15 }
+                        }
+                    }
+                    
+                    Button(action: {
+                        Task{
+                            if let id = selectedItem.id {
+                                await deleteSelectedItem(id: id) { success in
+                                    if success {
+                                        print("Successfully deleted the item")
+                                        isShowSelectedItem = false
+                                    } else {
+                                        print("Failed to delete the item")
+                                    }
+                                }
+                            } else {
+                                print("Error: selectedItem.id is nil")
+                            }
+                            
+                        }
+                    }) {
+                        Text("delete")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.red)
+                            .padding(5)
+                    }
 
+                }
             }
         }
         .padding()
         .background(Color.gray.opacity(0.1))
         .cornerRadius(20)
-        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: 200)
+        .frame(maxWidth: .infinity, maxHeight: 200)
     }
 }
 
